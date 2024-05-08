e3e313b6e1bb0ea09d914ec2c4036ed69b6b0c90
muhammad-alamin1 <muhammad.alamindev01@gmail.com>
2024-05-08 22:29:38 +0600

    print func

--- io_1.py
+++ io_1.py
@@ -1,1 +1,12 @@
-print("Hello", "World", sep=" & ", end=" ||")
+# print("Hello", "World", sep=" & ", end="\n")
+# string concat
+first_name = "Muhammad"
+last_name = "Al- AMin"
+
+full_name = first_name + " " + last_name
+# print(full_name)
+
+# print file 
+for i in range(5):
+    with open("python.txt", "w") as file:
+        print("Free Palestine", end=".", file=file, flush=False)
